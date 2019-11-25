4022a77b6d08b875006b8bf6d93c77d66addca78
1Jace1 <Jannik.Sp@web.de>
2019-11-25 11:54:07 +0100

    upload to git

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string name)
+        {
+            ExampleModel model = new ExampleModel();
+            model.Name = name;
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
